de0834fc471db7e61f27dffed0ca4000710c0474
sanjib1990 <devnath.sanjib@gmail.com>
2017-09-20 21:04:45 +0530

    Update slack bot

--- modules/slack_bot.js
+++ modules/slack_bot.js
@@ -1,35 +1,56 @@
+import date from "moment";
 import _ from "underscore";
 import {load} from "dotenv-safe";
 import {RtmClient} from "@slack/client";
 import {RTM_EVENTS} from "@slack/client";
 
+"use strict";
+
 load();
 
-let botToken = process.env.SLACK_BOT_TOKEN || "";
-let rtm = new RtmClient(botToken);
-let pattern = "Deploy Successfull on `{ip_address}` Branch: `{branch}` Codebase: `{website}`";
+let winston = require("winston"),
+    botToken = process.env.SLACK_BOT_TOKEN || "",
+    rtm = new RtmClient(botToken),
+    pattern = "{product}expected to reach {type}in {sec}secs sent at {timestamp}failed for id {orderId}";
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-    console.log('Message:', JSON.stringify(getPlaceholders(pattern, message.text)));
+    let matches = message.text.getPlaceholders(pattern);
+
+    if (empty(matches)) {
+        return false;
+    }
+
+    log("MESSAGE", matches);
 });
 
 rtm.start();
 
-function getPlaceholders(patternString, contextString)
+String.prototype.isSimilarTo = function (patternString)
 {
-    let patterns = patternString.match(/\{[A-Za-z0-9_\-\.]+\}/g);
+    patternString = patternString.replace(/\{[A-Za-z0-9_\-\.]+\}/g, '[A-Za-z0-9_\\-\\.\\s:]+');
 
-    let params = [];
+    return this.match(patternString);
+};
+
+String.prototype.getPlaceholders = function (patternString)
+{
+    if (! this.isSimilarTo(patternString)) {
+        return false;
+    }
+
+    let patterns = patternString.match(/\{[A-Za-z0-9_\-\.]+\}/g),
+        params = [];
+    
     for (let i=0; i< patterns.length; i++) {
-        let matching = patternString[patternString.indexOf(patterns[i]) + patterns[i].length];
-        let matchingstring = null;
-        let subString = contextString.substr(patternString.indexOf(patterns[i]));
+        let matching = patternString[patternString.indexOf(patterns[i]) + patterns[i].length],
+            matchingstring = null,
+            subString = this.substr(patternString.indexOf(patterns[i]));
 
         matchingstring = empty(matching) ? subString : subString.split(matching)[0];
 
         params.push({
             var: patterns[i].slice(1, -1),
-            value: matchingstring
+            value: matchingstring.trim()
         });
 
         patternString = patternString.replace(patterns[i], matchingstring);
@@ -42,9 +63,14 @@ function getPlaceholders(patternString, contextString)
     }, {});
 
     return params;
-}
+};
 
 function empty(item)
 {
-    return item === "" || item === null || item === undefined;
+    return item === "" || item === null || item === undefined || item === false;
+}
+
+function log(...args)
+{
+    winston.log('info', '['+ date().format('YYYY-MM-DD H:mm:ss') + ']', ...args);
 }
